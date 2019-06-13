9c12405b6dcb40c65aebc34b5f1fa2755dfff958
yeaze <36060454+yeaze@users.noreply.github.com>
2019-06-13 15:21:32 +0800

    修复语法错误

--- session.go
+++ session.go
@@ -14,7 +14,7 @@ type Session struct {
 
 // NewSession create a new session
 func NewSession(conn *Conn) *Session {
-	id, _ := uuid.NewV4()
+	id := uuid.NewV4()
 	session := &Session{
 		sID:      id.String(),
 		uID:      "",
